563d9a0b93853bb78c18cc9cddab22003b29fd8c
sajay <sajaykannan10@gmail.com>
2022-10-20 12:59:42 +0530

    query params

--- products/views.py
+++ products/views.py
@@ -8,7 +8,15 @@ from products.serializers import ProductSerializer
 
 class ProductsView(APIView):
     def get(self,request,*args,**kw):
+        
         qs=Mobiles.objects.all()
+        if "brand" in request.query_params:
+            qs=qs.filter(brand=request.query_params.get("brand"))
+        if "band" in request.query_params:
+            qs=qs.filter(band=request.query_params.get("band"))
+        
+
+
         serializer=ProductSerializer(qs,many=True)
         return Response(data=serializer.data)
     def post(self,request,*args,**kw):
@@ -27,8 +35,18 @@ class ProductDetailView(APIView):
         serilizer=ProductSerializer(qs,many=True)
         return Response(data=serilizer.data)
         
-       
-
-       
-
+    def delete(self,request,*args,**kw):
+        id=kw.get("id")
+        Mobiles.objects.filter(id=id).delete()
+        return Response(data="deleted")
     
+
+    def put(self,request,*args,**kw):
+        id=kw.get("id")
+        serializer=ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            Mobiles.objects.filter(id=id).update(**serializer.validated_data)
+            return Response(data=serializer.data)
+        else:
+            return Response(data=serializer.errors)
+
